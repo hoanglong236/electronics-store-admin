d7e4d29cd805e9d0432d28b7b808122f7af50dbf
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-03-13 17:22:52 +0700

    Fix bug coding

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -17,68 +17,57 @@ class BrandSeeder extends Seeder
         Brand::create([
             'name' => 'Acer',
             'slug' => 'acer',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/acer-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/acer.png',
         ]);
         Brand::create([
             'name' => 'Apple',
             'slug' => 'apple',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/apple-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/apple.png',
         ]);
         Brand::create([
             'name' => 'Asus',
             'slug' => 'asus',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/asus-logo.jpg',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/asus.jpg',
         ]);
         Brand::create([
             'name' => 'Dell',
             'slug' => 'dell',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/dell-logo.jpg',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/dell.jpg',
         ]);
         Brand::create([
             'name' => 'HP',
             'slug' => 'hp',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/hp-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/hp.png',
         ]);
         Brand::create([
             'name' => 'Huawei',
             'slug' => 'huawei',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/huawei-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/huawei.png',
         ]);
         Brand::create([
             'name' => 'Lenovo',
             'slug' => 'lenovo',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/lenovo-logo.jpg',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/lenovo.jpg',
         ]);
         Brand::create([
             'name' => 'MSI',
             'slug' => 'msi',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/msi-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/msi.png',
         ]);
         Brand::create([
             'name' => 'Oppo',
             'slug' => 'oppo',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/oppo-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/oppo.png',
         ]);
         Brand::create([
             'name' => 'Samsung',
             'slug' => 'samsung',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/samsung-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/samsung.png',
         ]);
         Brand::create([
             'name' => 'Xiaomi',
             'slug' => 'xiaomi',
-            'logo_path' => Constants::BRAND_LOGO_PATH . '/xiaomi-logo.png',
-            'delete_flag' => false,
+            'logo_path' => Constants::BRAND_LOGO_PATH . '/xiaomi.png',
         ]);
     }
 }
