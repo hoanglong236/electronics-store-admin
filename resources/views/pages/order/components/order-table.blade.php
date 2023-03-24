8ff5f5795845173108852e28a47bd875f8558548
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-03-24 12:34:33 +0700

    Fix bug UI for order module

--- resources/views/pages/order/components/order-table.blade.php
+++ resources/views/pages/order/components/order-table.blade.php
@@ -1,59 +1,62 @@
-<table class="table table-borderless table-data6 table-radius">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Customer</th>
-            <th>Delivery Address</th>
-            <th>Total</th>
-            <th>Status</th>
-            <th>Updated at</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($customOrders as $customOrder)
+<div class="table-responsive m-b-40">
+    <table class="table table-borderless table-data6 table-radius">
+        <thead>
             <tr>
-                <td>{{ $customOrder->id }}</td>
-                <td>
-                    {{ $customOrder->customer_name }}<br>
-                    {{ $customOrder->customer_email }}
-                </td>
-                <td>{{ $customOrder->delivery_address }}</td>
-                <td>{{ '$' . number_format($customOrder->total, 2) }}</td>
-                <td>
-                    @if (count($nextSelectableStatusMap[$customOrder->status]) === 0)
-                        <span @class([
-                            'order-cancelled' => $customOrder->status === 'Cancelled',
-                            'order-completed' => $customOrder->status === 'Completed',
-                        ])>{{ $customOrder->status }}</span>
-                    @else
-                        <form action="{{ route('manage.order.update-order-status', $customOrder->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <select name="status" class="form-control text--small" onchange="this.form.submit()">
-                                <option value="{{ $customOrder->status }}">{{ $customOrder->status }}</option>
-                                @foreach ($nextSelectableStatusMap[$customOrder->status] as $nextStatus)
-                                    <option value="{{ $nextStatus }}">{{ $nextStatus }}</option>
-                                @endforeach
-                            </select>
-                            @error('status')
-                                @if (intval(Session::get('orderId')) === $customOrder->id)
-                                    <div class="alert alert-danger mt-1 text--small" role="alert">{{ $message }}
-                                    </div>
-                                @endif
-                            @enderror
-                        </form>
-                    @endif
-                </td>
-                <td>{{ $customOrder->updated_at }}</td>
-                <td>
-                    <a href="{{ route('manage.order.detail', $customOrder->id) }}">
-                        <button type="submit" class="btn btn-info btn-sm icon-button">
-                            <i class="fa fa-info-circle"></i>
-                        </button>
-                    </a>
-                </td>
+                <th>ID</th>
+                <th>Customer</th>
+                <th>Delivery Address</th>
+                <th>Total</th>
+                <th>Status</th>
+                <th>Updated at</th>
+                <th></th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($customOrders as $customOrder)
+                <tr>
+                    <td>{{ $customOrder->id }}</td>
+                    <td>
+                        {{ $customOrder->customer_name }}<br>
+                        {{ $customOrder->customer_email }}
+                    </td>
+                    <td>{{ $customOrder->delivery_address }}</td>
+                    <td>{{ '$' . number_format($customOrder->total, 2) }}</td>
+                    <td>
+                        @if (count($nextSelectableStatusMap[$customOrder->status]) === 0)
+                            <span @class([
+                                'order-cancelled' => $customOrder->status === 'Cancelled',
+                                'order-completed' => $customOrder->status === 'Completed',
+                            ])>{{ $customOrder->status }}</span>
+                        @else
+                            <form action="{{ route('manage.order.update-order-status', $customOrder->id) }}"
+                                method="POST">
+                                @csrf
+                                @method('PUT')
+                                <select name="status" class="form-control text--small" onchange="this.form.submit()">
+                                    <option value="{{ $customOrder->status }}">{{ $customOrder->status }}</option>
+                                    @foreach ($nextSelectableStatusMap[$customOrder->status] as $nextStatus)
+                                        <option value="{{ $nextStatus }}">{{ $nextStatus }}</option>
+                                    @endforeach
+                                </select>
+                                @error('status')
+                                    @if (intval(Session::get('orderId')) === $customOrder->id)
+                                        <div class="alert alert-danger mt-1 text--small" role="alert">{{ $message }}
+                                        </div>
+                                    @endif
+                                @enderror
+                            </form>
+                        @endif
+                    </td>
+                    <td>{{ $customOrder->updated_at }}</td>
+                    <td>
+                        <a href="{{ route('manage.order.details', $customOrder->id) }}">
+                            <button type="submit" class="btn btn-info btn-sm icon-button">
+                                <i class="fa fa-info-circle"></i>
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
--- resources/views/pages/order/orders.blade.php
+++ resources/views/pages/order/orders.blade.php
@@ -5,7 +5,9 @@
         <div class="col-md-12">
             <h2 class="title-5 mb-4">Order</h2>
             @if (Session::has(Constants::ACTION_SUCCESS))
-                @include('shared.components.operation-success-alert', ['succeeMessage', Session::get(Constants::ACTION_SUCCESS)])
+                @include('shared.components.action-success-label', [
+                    'succeeMessage' => Session::get(Constants::ACTION_SUCCESS),
+                ])
             @endif
 
             <div class="table-data__tool">
@@ -45,9 +47,7 @@
 
     <div class="row m-t-10">
         <div class="col-md-12">
-            <div class="table-responsive m-b-40">
-                @include('pages.order.components.order-table')
-            </div>
+            @include('pages.order.components.order-table')
         </div>
     </div>
 @endsection
