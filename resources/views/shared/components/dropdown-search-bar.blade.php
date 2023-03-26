accfe4b13cf6e23f16ac215bc0842bb6d7e468c5
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-03-26 21:56:09 +0700

    Fix bug UI

--- resources/views/shared/components/dropdown-search-bar.blade.php
+++ resources/views/shared/components/dropdown-search-bar.blade.php
@@ -1,13 +1,12 @@
 <form class="form-header" action="{{ $searchFormUrl }}" method="GET">
     <div class="search-bar-wrapper">
         <select class="search-select-input" name="searchOption">
-            @foreach ($searchOptionMap as $key => $value) {
+            @foreach ($searchOptionMap as $key => $value)
                 <option value="{{ $key }}" @selected($key === $searchOption)>{{ $value }}</option>
             @endforeach
         </select>
-        <input class="au-input au-input--xl au-input--custom" type="text" name="searchKeyword"
-            value="{{ $searchKeyword }}" placeholder="Search..."
-            maxlength="{{ $searchKeywordMaxLength ?? Constants::SEARCH_KEYWORD_MAX_LENGTH }}">
+        <input class="au-input au-input--xl" type="text" name="searchKeyword" value="{{ $searchKeyword }}"
+            placeholder="Search..." maxlength="{{ $searchKeywordMaxLength ?? Constants::SEARCH_KEYWORD_MAX_LENGTH }}">
         <button class="au-btn--submit" type="submit">
             <i class="zmdi zmdi-search"></i>
         </button>
--- resources/views/shared/components/search-bar.blade.php
+++ resources/views/shared/components/search-bar.blade.php
@@ -1,6 +1,6 @@
 <form class="form-header" action="{{ $searchFormUrl }}" method="GET">
     <input class="au-input au-input--xl" type="text" name="searchKeyword" value="{{ $searchKeyword }}"
-        placeholder="Search brand name..." maxlength="64">
+        placeholder="Search..." maxlength="{{ $searchKeywordMaxLength ?? Constants::SEARCH_KEYWORD_MAX_LENGTH }}">
     <button class="au-btn--submit" type="submit">
         <i class="zmdi zmdi-search"></i>
     </button>
