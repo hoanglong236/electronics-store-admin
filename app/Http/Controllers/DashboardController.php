b6bffcb62cc5c49d8edf82f9b72f32390dd203fb
hoanglong236 <99656288+hoanglong236@users.noreply.github.com>
2023-03-26 16:38:36 +0700

    Fix bug for Dashboard module

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('pages.dashboard', ['pageTitle' => 'Dashboard']);
+    public function index()
+    {
+        $data = [
+            'pageTitle' => 'Dashboard'
+        ];
+
+        return view('pages.dashboard', ['data' => $data]);
     }
 }
