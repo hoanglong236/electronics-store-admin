e9df267b715690609ee847faba2dd3d8f59e174b
hoanglong236 <hahahalalala236@gmail.com>
2023-05-12 00:56:54 +0700

    Build dashboard module #26: - Handle responsive for search form

--- resources/views/pages/dashboard/components/dashboard-search-form.blade.php
+++ resources/views/pages/dashboard/components/dashboard-search-form.blade.php
@@ -1,15 +1,18 @@
 <form action="{{ $searchUrl }}" method="GET">
     <div class="dashboard-search-form-wrapper">
-        <div class="dashboard-form-group">
-            <label for="fromDate">From:</label>
-            <input id="fromDate" type="date" name="fromDate" value="{{ $fromDate }}" class="form-control" required>
+        <div class="dashboard-search-form-fields">
+            <div class="dashboard-form-group">
+                <label for="fromDate" class="dashboard-search-date-label">From:</label>
+                <input id="fromDate" type="date" name="fromDate" value="{{ $fromDate }}" class="form-control"
+                    required>
+            </div>
+            <div class="dashboard-form-group">
+                <label for="toDate" class="dashboard-search-date-label">To:</label>
+                <input id="toDate" type="date" name="toDate" value="{{ $toDate }}" class="form-control"
+                    required>
+            </div>
         </div>
-        <div class="dashboard-form-group">
-            <label for="toDate">To:</label>
-            <input id="toDate" type="date" name="toDate" value="{{ $toDate }}" class="form-control"
-                required>
-        </div>
-        <div class="dashboard-form-action">
+        <div class="dashboard-form-search-actions">
             <button type="submit" class="dashboard-search-btn">SEARCH</button>
         </div>
     </div>
