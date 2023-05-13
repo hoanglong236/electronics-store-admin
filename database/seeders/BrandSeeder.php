e0d9cff860944cc28c468334951d42db993057b2
hoanglong236 <hahahalalala236@gmail.com>
2023-05-13 14:32:42 +0700

    Fix bug brand module #5

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -14,71 +14,31 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        Brand::create([
-            'name' => 'Acer',
-            'slug' => 'acer',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/acer.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Apple',
-            'slug' => 'apple',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/apple.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Asus',
-            'slug' => 'asus',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/asus.jpg',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Dell',
-            'slug' => 'dell',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/dell.jpg',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'HP',
-            'slug' => 'hp',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/hp.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Huawei',
-            'slug' => 'huawei',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/huawei.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Lenovo',
-            'slug' => 'lenovo',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/lenovo.jpg',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'MSI',
-            'slug' => 'msi',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/msi.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Oppo',
-            'slug' => 'oppo',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/oppo.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Samsung',
-            'slug' => 'samsung',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/samsung.png',
-            'delete_flag' => false,
-        ]);
-        Brand::create([
-            'name' => 'Xiaomi',
-            'slug' => 'xiaomi',
-            'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/xiaomi.png',
-            'delete_flag' => false,
-        ]);
+        $brandInfoArray = [
+            ['name' => 'Acer', 'slug' => 'acer'],
+            ['name' => 'Apple', 'slug' => 'apple'],
+            ['name' => 'Asus', 'slug' => 'asus'],
+            ['name' => 'Dell', 'slug' => 'dell'],
+            ['name' => 'HP', 'slug' => 'hp'],
+            ['name' => 'Huawei', 'slug' => 'huawei'],
+            ['name' => 'Lenovo', 'slug' => 'lenovo'],
+            ['name' => 'MSI', 'slug' => 'msi'],
+            ['name' => 'Oppo', 'slug' => 'oppo'],
+            ['name' => 'Samsung', 'slug' => 'samsung'],
+            ['name' => 'Xiaomi', 'slug' => 'xiaomi'],
+        ];
+        $this->generateBrands($brandInfoArray);
+    }
+
+    private function generateBrands($brandInfoArray)
+    {
+        foreach ($brandInfoArray as $brandInfo) {
+            Brand::create([
+                'name' => $brandInfo['name'],
+                'slug' => $brandInfo['slug'],
+                'logo_path' => Config::FOLDER_PATH_BRAND_LOGO . '/' . $brandInfo['slug'] . '.png',
+                'delete_flag' => false,
+            ]);
+        }
     }
 }
