9455e7c6d5f1c208f30ce9cd7ef471dbef598fb9
hoanglong236 <hahahalalala236@gmail.com>
2023-06-05 00:27:19 +0700

    Fix bug product module #8

--- app/Repositories/IProductRepository.php
+++ app/Repositories/IProductRepository.php
@@ -5,7 +5,6 @@ namespace App\Repositories;
 interface IProductRepository
 {
     public function findById(int $id);
-    public function getCustomProductById(int $id);
     public function create(array $attributes);
     public function update(array $attributes, int $id);
     public function deleteById(int $id);
@@ -14,6 +13,7 @@ interface IProductRepository
         string $searchOption, string $escapedKeyword, int $itemPerPage
     );
 
+    public function getCustomProductById(int $id);
     public function retrieveProductImagesByProductId(int $productId);
     public function createProductImage(array $attributes);
     public function deleteProductImageById(int $id);
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -5,7 +5,6 @@ namespace App\Repositories;
 use App\DataFilterConstants\ProductSearchOptionConstants;
 use App\Models\Product;
 use App\Models\ProductImage;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 
 class ProductRepository implements IProductRepository
@@ -13,13 +12,6 @@ class ProductRepository implements IProductRepository
     public function findById(int $id)
     {
         return Product::where(['id' => $id, 'delete_flag' => false])
-            ->first();
-    }
-
-    public function getCustomProductById(int $id)
-    {
-        return $this->getCustomProductQueryBuilder()
-            ->where('products.id', $id)
             ->first();
     }
 
@@ -48,40 +40,11 @@ class ProductRepository implements IProductRepository
         return false;
     }
 
-    public function paginateCustomProducts(int $itemPerPage)
+    private function getSearchProductsByAllQueryBuilder(string $escapedKeyword)
     {
-        return $this->getCustomProductQueryBuilder()
-            ->latest()
-            ->paginate($itemPerPage);
-    }
-
-    public function searchCustomProductsAndPaginate(
-        string $searchOption, string $escapedKeyword, int $itemPerPage
-    ) {
-        $queryBuilder = null;
-        switch ($searchOption) {
-            case ProductSearchOptionConstants::SEARCH_ALL:
-                $queryBuilder = $this->getSearchCustomProductsByAllQueryBuilder($escapedKeyword);
-                break;
-            case ProductSearchOptionConstants::SEARCH_CATEGORY:
-                $queryBuilder = $this->getSearchCustomProductsByCategoryQueryBuilder($escapedKeyword);
-                break;
-            case ProductSearchOptionConstants::SEARCH_BRAND:
-                $queryBuilder = $this->getSearchCustomProductsByBrandQueryBuilder($escapedKeyword);
-                break;
-        }
-
-        if (is_null($queryBuilder)) {
-            return new LengthAwarePaginator([], 0, $itemPerPage);
-        }
-
-        return $queryBuilder->latest()
-            ->paginate($itemPerPage);
-    }
-
-    private function getSearchCustomProductsByAllQueryBuilder(string $escapedKeyword)
-    {
-        return $this->getCustomProductQueryBuilder()
+        return DB::table('products')
+            ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->join('brands', 'brands.id', '=', 'products.brand_id')
             ->where(function ($query) use ($escapedKeyword) {
                 $query->Where('products.name', 'LIKE', '%' . $escapedKeyword . '%')
                     ->orWhere('products.slug', 'LIKE', '%' . $escapedKeyword . '%')
@@ -92,25 +55,71 @@ class ProductRepository implements IProductRepository
             });
     }
 
-    private function getSearchCustomProductsByCategoryQueryBuilder(string $escapedKeyword)
+    private function getSearchProductsByCategoryQueryBuilder(string $escapedKeyword)
     {
-        return $this->getCustomProductQueryBuilder()
+        return DB::table('products')
+            ->join('categories', 'categories.id', '=', 'products.category_id')
             ->where(function ($query) use ($escapedKeyword) {
                 $query->Where('categories.name', 'LIKE', '%' . $escapedKeyword . '%')
                     ->orWhere('categories.slug', 'LIKE', '%' . $escapedKeyword . '%');
             });
     }
 
-    private function getSearchCustomProductsByBrandQueryBuilder(string $escapedKeyword)
+    private function getSearchProductsByBrandQueryBuilder(string $escapedKeyword)
     {
-        return $this->getCustomProductQueryBuilder()
+        return DB::table('products')
+            ->join('brands', 'brands.id', '=', 'products.brand_id')
             ->where(function ($query) use ($escapedKeyword) {
                 $query->Where('brands.name', 'LIKE', '%' . $escapedKeyword . '%')
                     ->orWhere('brands.slug', 'LIKE', '%' . $escapedKeyword . '%');
             });
     }
 
-    private function getCustomProductQueryBuilder()
+    private function getSearchProductsQueryBuilder(
+        string $searchOption = null,
+        string $escapedKeyword = null
+    ) {
+        $queryBuilder = null;
+        if (!is_null($escapedKeyword) && strlen($escapedKeyword) > 0) {
+            switch ($searchOption) {
+                case ProductSearchOptionConstants::SEARCH_ALL:
+                    $queryBuilder = $this->getSearchProductsByAllQueryBuilder($escapedKeyword);
+                    break;
+                case ProductSearchOptionConstants::SEARCH_CATEGORY:
+                    $queryBuilder = $this->getSearchProductsByCategoryQueryBuilder($escapedKeyword);
+                    break;
+                case ProductSearchOptionConstants::SEARCH_BRAND:
+                    $queryBuilder = $this->getSearchProductsByBrandQueryBuilder($escapedKeyword);
+                    break;
+            }
+        }
+        $queryBuilder = $queryBuilder ?? DB::table('products');
+        return $queryBuilder->select(
+            'products.id',
+            'products.name',
+            'products.price',
+            'products.discount_percent',
+            'products.quantity',
+            'products.main_image_path'
+        );
+    }
+
+    public function paginateCustomProducts(int $itemPerPage)
+    {
+        return $this->getSearchProductsQueryBuilder()
+            ->latest('id')
+            ->paginate($itemPerPage);
+    }
+
+    public function searchCustomProductsAndPaginate(
+        string $searchOption, string $escapedKeyword, int $itemPerPage
+    ) {
+        return $this->getSearchProductsQueryBuilder($searchOption, $escapedKeyword)
+            ->latest('id')
+            ->paginate($itemPerPage);
+    }
+
+    public function getCustomProductById(int $id)
     {
         return DB::table('products')
             ->join('categories', 'categories.id', '=', 'products.category_id')
@@ -120,7 +129,8 @@ class ProductRepository implements IProductRepository
                 'categories.name as category_name',
                 'brands.name as brand_name',
             )
-            ->where('products.delete_flag', false);
+            ->where(['products.id' => $id, 'products.delete_flag' => false])
+            ->first();
     }
 
     public function retrieveProductImagesByProductId(int $productId)
